d856b3443b285eb348b3e89f198b1f30fc732940
Karen Flores <karen.ag.flores@gmail.com>
2020-04-22 22:01:03 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,9 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+<<<<<<< HEAD
   has_many :song_genre
   has_many :genres, through: :song_genre
+=======
+  has_many :genres
+>>>>>>> a48701a23997e943f47223e2db00fdccafacad9a
 end
